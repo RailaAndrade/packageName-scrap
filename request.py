b1380306aca1a2bf4facf5cc08a26102ae1d2108
Raila Alves de Andrade <Raila@raila.local>
2022-05-31 21:03:10 -0300

    adiciona multitthreading na requisicao

--- request.py
+++
@@ -1,5 +0,0 @@
-import requests
-url= 'https://play.google.com/store/apps/details?id=com.google.android.youtube'
-r = requests.get(url)
-
-print (r.text)
--- requisicao.py
+++ requisicao.py
@@ -5,17 +5,24 @@ from bs4 import BeautifulSoup
 import selenium
 from lxml import html
 import csv
+from threading import Thread, Barrier
+from queue import Queue
+import time
+import timeit
 #leitura do arquivo
+
+inicio = timeit.default_timer()
 file = open("tabela_app_profile_TOP.csv")
 csvreader = csv.reader(file)
 header = next(csvreader)
 print(header)
 rows = []
+newrow=[]
 
 for row in csvreader:
     rows.append(row)
 file.close()
-#print(rows)
+print(rows)
 
 url = []
 #lista
@@ -26,24 +33,58 @@ for i in range(length-1):
     url.append('https://play.google.com/store/apps/details?id=' + rows[i][0])
 
 #requests
+concurrent = 5
+s=1
+def doWork():
+    
+    while True:
+        url = q.get()
+        crawl(url)
+        q.task_done()
+     
 
-for i in range(length-1):
+
+def crawl(myurl):
     try:
-        names = '//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div/main/c-wiz[1]/c-wiz[1]/div/div[2]/div/div[1]/c-wiz[1]/h1/span//text()'
-        categories='//*[@id="fcxH9b"]/div[4]/c-wiz/div/div[2]/div/div/main/c-wiz[1]/c-wiz[1]/div/div[2]/div/div[1]/div[1]/div[1]/div[1]/span[2]/a//text()'
-        r = requests.get(url[i])
+        names = '//*[@id="yDmH0d"]/c-wiz[2]/div/div/div[1]/div[1]/div/div/c-wiz/div[2]/div[1]/div/h1/span//text()'     
+        r = requests.get(myurl)
         tree = html.fromstring(r.content)
+        newrow.append({"name":tree.xpath(names)[0]})
         print(tree.xpath(names)[0])
-        rows[i].append(tree.xpath(names)[0])
-        rows[i].append(tree.xpath(categories)[0])
-        print(i)
     except:
-        rows[i].append('')
-        rows[i].append('')
-        #print('ERRO')
-        print(i)
+        newrow.append({"name":''})
+        print('ERRO')
+        pass
+       
+
+
+q = Queue(concurrent * 2)
+
+for i in range(concurrent):
+    t = Thread(target=doWork)
+    t.daemon = True
+    t.start()
+try:
+
+    for x in range(length-1):
+        lin= url[x]
+        q.put(lin.strip())
+    q.join()
+ 
+except KeyboardInterrupt:
+    pass
+
+
+print("writing")
+for m in range(length-1):
+    if newrow[m]["name"]:
+        rows[m].append(newrow[m]["name"])
+
 
 with open('names.csv', 'w', encoding='UTF8', newline='') as f:
     writer = csv.writer(f)
-    writer.writerow(['package_name', 'freq', 'name', 'category'])
+    writer.writerow(['package_name', 'freq', 'name'])
     writer.writerows(rows)
+
+fim = timeit.default_timer()
+print ('duracao: %f' % (fim - inicio))
